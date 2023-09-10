82a3618bb89a58bd14ad5deedfde84c1a6faf669
zararafridi <zararafridi9091@gmail.com>
2023-09-10 17:13:52 +0500

    backend added

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+dotenv.config()
+
+
+
+mongoose.connect(process.env.MONGO_URL).then( ()=>{
+    console.log("mongose connection succesfull")}).catch((err)=>{
+            console.log(err)
+        }
+    );
+
+    app.use(express.json())
+    app.use("/api/users", userRoute);
+    app.use("/api/auth", authRoute);
+
+
+    app.listen(5000,()=>{
+        console.log("backend server is running ")
+    })
